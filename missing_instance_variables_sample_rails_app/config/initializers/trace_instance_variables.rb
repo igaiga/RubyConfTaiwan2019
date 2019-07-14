d7632e55360a4997de32d8ba6bfe703551ef17c3
Kuniaki Igarashi <igaiga@gmail.com>
2019-07-14 14:27:22 +0900

    Traslate to English in missing_instance_variables rails_app

--- missing_instance_variables_sample_rails_app/config/initializers/trace_instance_variables.rb
+++ missing_instance_variables_sample_rails_app/config/initializers/trace_instance_variables.rb
@@ -1,24 +1,24 @@
 def tracer(target_instance_variable_name)
   TracePoint.trace(:line) do |tp|
-    # ソース取得
+    # Get source code
     begin
       line = File.open(tp.path, "r"){|f| f.readlines[tp.lineno - 1] }
     rescue Errno::ENOENT => _e
     end
     next unless line
-    # erb対応
+    # Adopt to erb
     if match_data = line.match(/\A\s*<%=*(.*)%>\s*\z/)
       line = match_data.captures.first
     end
-    # AST取得
+    # Get AST
     begin
       node = RubyVM::AbstractSyntaxTree.parse(line).children.last
     rescue SyntaxError => _e
       next
     end
-    # インスタンス変数への代入かを調べる
+    # Check the AST is instance variable assignment
     next unless node.type == :IASGN
-    # インスタンス変数名を調べる
+    # Check instance variable name
     instance_variable_name = node.children.first
     next unless instance_variable_name == target_instance_variable_name.to_sym
     puts "#{target_instance_variable_name} is assigned in #{tp.path}:#{tp.lineno} #{tp.method_id} #{tp.defined_class}"
