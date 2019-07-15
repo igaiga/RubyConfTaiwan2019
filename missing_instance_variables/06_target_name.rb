9ed4ebdb0a44555be332d95a183de0a9640f2533
Kuniaki Igarashi <igaiga@gmail.com>
2019-07-16 08:17:44 +0900

    Update codes

--- missing_instance_variables/06_target_name.rb
+++ missing_instance_variables/06_target_name.rb
@@ -4,7 +4,7 @@ class User
   end
 end
 
-def tracer(target_class_name)
+def tracer(target_instance_variable_name)
   TracePoint.trace(:line) do |tp|
     # Get source code
     line = File.open(tp.path, "r"){|f| f.readlines[tp.lineno - 1] }
@@ -12,13 +12,13 @@ def tracer(target_class_name)
     node = RubyVM::AbstractSyntaxTree.parse(line).children.last
     # Check the AST is instance variable assignment
     next unless node.type == :IASGN
-    # Check class
-    target_class = Kernel.const_get(target_class_name)
-    next unless tp.self.is_a?(target_class)
+    # Check instance variable name
+    instance_variable_name = node.children.first
+    next unless instance_variable_name == target_instance_variable_name.to_sym
     puts "[TP:#{tp.event}] #{tp.path}:#{tp.lineno} #{tp.method_id} #{tp.defined_class}"
   end
 end
 
-tracer("User")
+tracer("@hi")
 User.new.hi
-#=> [TP:line] 05_ast2.rb:3 hi User
+#=> [TP:line] 06_target_name.rb:3 hi User
--- missing_instance_variables/07_tagert_class.rb
+++ missing_instance_variables/07_tagert_class.rb
@@ -10,18 +10,18 @@ def tracer(target_class_name, target_instance_variable_name)
     line = File.open(tp.path, "r"){|f| f.readlines[tp.lineno - 1] }
     # Get AST
     node = RubyVM::AbstractSyntaxTree.parse(line).children.last
-    # Check AST is instance variable assignment
+    # Check the AST is instance variable assignment
     next unless node.type == :IASGN
-    # Check class name
-    target_class = Kernel.const_get(target_class_name)
-    next unless tp.self.is_a?(target_class)
     # Check instance variable name
     instance_variable_name = node.children.first
     next unless instance_variable_name == target_instance_variable_name.to_sym
+    # Check class
+    target_class = Kernel.const_get(target_class_name)
+    next unless tp.self.is_a?(target_class)
     puts "[TP:#{tp.event}] #{tp.path}:#{tp.lineno} #{tp.method_id} #{tp.defined_class}"
   end
 end
 
 tracer("User", "@hi")
 User.new.hi
-#=> [TP:line] 07_ast4.rb:3 hi User
+#=> [TP:line] 07_target_class.rb:3 hi User
